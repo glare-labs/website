737a22b73df480ae54d27ac53498175bce82ef8b
Bre 97 <bre97.web@gmail.com>
2024-11-03 19:58:30 +0800

    fix: refactor search-input component

--- src/modules/blogs-module/index.ts
+++ src/modules/blogs-module/index.ts
@@ -6,8 +6,10 @@ import Footer from './components/footer/footer.astro'
 import Header from './components/header/header.astro'
 import NavigationBar from './components/navigation/navigation-bar.astro'
 import NavigationRail from './components/navigation/navigation-rail.astro'
+import SearchInput from './components/search-blog/search-input.astro'
 import ProductBlogLayout from './layouts/product-blog-layout.astro'
 import ProductBlogsBodyLayout from './layouts/product-blogs-body-layout.astro'
 import ProductTopDrawerLayout from './layouts/product-top-drawer-layout.astro'
 
-export { Footer, Header, NavigationBar, NavigationRail, ProductBlogLayout, ProductBlogsBodyLayout, ProductTopDrawerLayout }
+
+export { Footer, Header, NavigationBar, NavigationRail, ProductBlogLayout, ProductBlogsBodyLayout, ProductTopDrawerLayout, SearchInput }
