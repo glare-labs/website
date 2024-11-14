27cd0416bb8e9f32d808185697024af461a53401
Bre 97 <bre97.web@gmail.com>
2024-11-14 13:21:39 +0800

    refactor: navigation-drawer module

---
+++ src/modules/navigation-drawer/index.ts
@@ -0,0 +1,10 @@
+
+import NavigationDrawerCloseButton from './components/navigation-drawer-close-button.astro'
+import NavigationDrawerHeader from './components/navigation-drawer-header.astro'
+import NavigationDrawerOpenButton from './components/navigation-drawer-open-button.astro'
+import NavigationDrawer from './components/navigation-drawer.astro'
+export {
+    NavigationDrawer,
+    NavigationDrawerCloseButton,
+    NavigationDrawerHeader, NavigationDrawerOpenButton
+}
