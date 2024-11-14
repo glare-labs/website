a60574ad6d8d6fc5f438f70990fc4bac88fda227
Bre 97 <bre97.web@gmail.com>
2024-11-14 13:23:37 +0800

    chore: rename glare-labs module

---
+++ src/modules/glare-labs-module/index.ts
@@ -0,0 +1,10 @@
+
+import GlareLabsLogo from './components/glare-labs-logo.astro'
+import ProductFooter from './components/product-footer.astro'
+import ProductHeader from './components/product-header.astro'
+import HeaderLayout from './layouts/header-layout.astro'
+
+export {
+    GlareLabsLogo,
+    HeaderLayout, ProductFooter, ProductHeader
+}
--- src/modules/glare-labs/index.ts
+++
@@ -1,6 +0,0 @@
-
-import GlareLabsLogo from './components/glare-labs-logo.astro'
-
-export {
-    GlareLabsLogo
-}
