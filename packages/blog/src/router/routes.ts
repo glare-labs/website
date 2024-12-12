900a71b5848e11acd50e157bd230d4ba5a7eb74f
Bre 97 <bre97.web@gmail.com>
2024-12-12 16:26:36 +0800

    feat: i18n

--- packages/blog/src/router/routes.ts
+++ packages/blog/src/router/routes.ts
@@ -1,47 +1,86 @@
+import { getLangFromUrl, useTranslations } from "@/i18n/utils"
+import ChannelSvg from '@material-design-icons/svg/round/apps.svg?raw'
 import BookSvg from '@material-design-icons/svg/round/book.svg?raw'
-import CodeSvg from '@material-design-icons/svg/round/code.svg?raw'
 import HomeSvg from '@material-design-icons/svg/round/home.svg?raw'
 import SettingsSvg from '@material-design-icons/svg/round/settings.svg?raw'
 import { atom } from 'nanostores'
 
-export const routes = [
-    {
-        id: 0,
-        label: 'Home',
-        iconString: HomeSvg,
-        url: `/`,
-    },
-    {
-        id: 1,
-        label: 'Library',
-        iconString: BookSvg,
-        url: `/library/`,
-    },
-    {
-        id: 2,
-        label: 'Toys',
-        iconString: CodeSvg,
-        url: `/toys/`,
-    },
-    {
-        id: 3,
-        label: 'Settings',
-        iconString: SettingsSvg,
-        url: `/settings/`,
-    },
-]
+type TRoute = {
+    id: number
+    label: string
+    url: string
+    navigable: boolean
+    iconSvg?: string
+    children?: Array<TRoute>
+}
 
-export const currentRouteUrl = atom(routes[0].url)
+export const routes: (astroUrl: URL) => Array<TRoute> = (astroUrl: URL) => {
+    const lang = getLangFromUrl(astroUrl)
+    const t = useTranslations(lang)
+
+    return ([
+        {
+            id: 0,
+            navigable: true,
+            label: t('navigation.home'),
+            iconSvg: HomeSvg,
+            url: `/`,
+        },
+        {
+            id: 1,
+            navigable: true,
+            label: t('navigation.library'),
+            iconSvg: BookSvg,
+            url: `/library/`,
+        },
+        {
+            id: 2,
+            navigable: true,
+            label: t('navigation.channel'),
+            iconSvg: ChannelSvg,
+            url: `/channels/`,
+            children: [
+                {
+                    navigable: false,
+                    id: 0,
+                    label: 'Articles',
+                    url: '/channel/articles/'
+                },
+            ]
+        },
+        {
+            id: 3,
+            navigable: true,
+            label: t('navigation.settings'),
+            iconSvg: SettingsSvg,
+            url: `/settings/`,
+        },
+    ])
+}
+
+export const currentRouteUrl = atom(`/`)
 
 if (typeof window !== 'undefined') {
 
     window.addEventListener('DOMContentLoaded', () => {
         const url = new URL(window.location.href)
-        for (const route of routes) {
-            if (
-                route.url === url.pathname
-            ) {
-                currentRouteUrl.set(route.url)
+        const links = routes(url)
+
+        /**
+         * @example
+         * [, zh-TW, settings]
+         * [, en-US, settings]
+         */
+        const [, lang, real] = url.pathname.split('/')
+        console.log(lang, real)
+
+        if (real) {
+            for (const route of links) {
+                if (
+                    route.url === `/${real}/`
+                ) {
+                    currentRouteUrl.set(route.url)
+                }
             }
         }
     })
